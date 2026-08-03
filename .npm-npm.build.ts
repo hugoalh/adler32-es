c75e2d58811834d31bb866b1814a3aa92a4e1e9d
hugoalh <hugoalh@gmail.com>
2026-08-03 18:07:51 +0800

    Update resources

--- .npm-npm.build.ts
+++ .npm-npm.build.ts
@@ -1,7 +1,9 @@
-import { invokeDenoNodeJSTransformer } from "DNT";
-import { parse as parseJSONC } from "STD_JSONC";
-const jsrManifest = parseJSONC(await Deno.readTextFile(new URL(import.meta.resolve("./jsr.jsonc"))));
-await invokeDenoNodeJSTransformer({
+import {
+	readManifest,
+	transform
+} from "DNT";
+const manifest = await readManifest("jsr.jsonc");
+await transform({
 	copyEntries: [
 		"LICENSE.md",
 		"README.md"
@@ -10,13 +12,13 @@ await invokeDenoNodeJSTransformer({
 		"adler32": "./cli.ts"
 	},
 	//@ts-ignore Lazy type.
-	entrypointsScript: jsrManifest.exports,
+	entrypointsScript: manifest.exports,
 	generateDeclarationMap: true,
 	metadata: {
 		//@ts-ignore Lazy type.
-		name: jsrManifest.name,
+		name: manifest.name,
 		//@ts-ignore Lazy type.
-		version: jsrManifest.version,
+		version: manifest.version,
 		description: "A module to get the checksum of the data with algorithm Adler32.",
 		keywords: [
 			"adler32",
